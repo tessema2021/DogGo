22bc0bfd6f0d33fe0a86644009bc72c13996f011
tessema2021 <ninafuchere@gmail.com>
2021-12-13 19:24:19 -0600

    first commit

---
+++ DogGO/Repositories/IOwnerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogGO.Models;
+
+namespace DogGO.Repositories
+{
+   
+        public interface IOwnerRepository
+        {
+            List<Owner> GetAllOwners();
+            Owner GetOwenrById(int id);
+        Owner GetOwnerById(int id);
+    }
+    
+}
---
+++ DogGO/Repositories/IWalkerRepository.cs
@@ -0,0 +1,12 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace DogGO.Repositories
+{
+    public interface IWalkerRepository
+    {
+        List<Walker> GetAllWalkers();
+        Walker GetWalkerById(int id);
+    }
+}
